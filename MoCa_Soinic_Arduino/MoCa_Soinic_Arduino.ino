420195c07473bde19ee0eac573018f52d9ff6fcc
Chun Yan Liu <chunyan.liu@yannest.net>
2016-08-18 01:59:56 +0800

    update Digital Pin
    update Digital Pin

--- MoCa_Soinic_Arduino/MoCa_Soinic_Arduino.ino
+++ MoCa_Soinic_Arduino/MoCa_Soinic_Arduino.ino
@@ -1,16 +1,17 @@
 #include <Ultrasonic.h>
 #define TRIGGER_PIN  8
 #define ECHO_PIN     9
-#define TRIGGER_PIN2  12
-#define ECHO_PIN2     11
+#define TRIGGER_PIN2  11
+#define ECHO_PIN2     10
 
 Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
 Ultrasonic ultrasonic2(TRIGGER_PIN2, ECHO_PIN2);
 
 const int trig = 8;
 const int ech = 9;
-const int trig2 = 12;
-const int ech2 = 11;
+const int trig2 = 11;
+const int ech2 = 10;
+
 const int inter_time = 2000;
 int time = 0;
 
